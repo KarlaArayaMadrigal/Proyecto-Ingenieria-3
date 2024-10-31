974f8bd9ffac8dec39c7583e439506d1fd582793
Plataformas Abiertas <kd.araya.m@gmail.com>
2024-10-30 23:58:14 -0600

    Cambios visuales

--- views/header.php
+++ views/header.php
@@ -10,17 +10,42 @@
     <!-- jQuery y Bootstrap JS -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Share+Tech+Mono&display=swap" rel="stylesheet">
+
+<!--Fuente de body -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Share+Tech+Mono&display=swap" rel="stylesheet">
 </head>
 <body>
 <header>
+    <img
+        src="./images/logo.png"
+        class="img-fluid logo-icon"
+        alt="logo"
+    />
+    
     <h1>TechServer Solutions</h1>
+    <img 
+        src="./images/carrito.png"
+        class="img-fluid carrito-icon"
+        alt="carrito"
+        onclick="location.href='carrito.php'"  
+    />
+    <img 
+        src="./images/perfil.png"
+        class="img-fluid perfil-icon"
+        alt="perfil"
+        onclick="location.href='logout.php'" 
+    />
     <nav>
         <a href="index.php">Inicio</a>
-        <a href="productos.php">Productos</a>
-        <a href="carrito.php">Carrito</a>
-        <a href="contacto.php">Contáctenos</a>
         <a href="quienes_somos.php">Quiénes Somos</a>
-        <a href="logout.php">Cerrar sesión</a>
+        <a href="productos.php">Productos</a>
+        <a href="contacto.php">Contáctenos</a>
+        
     </nav>
 </header>
 <main>
